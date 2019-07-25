f6be552ebf163ff5964903d08162ef4bde76c085
tuzmusic <tuzmusic@gmail.com>
2019-07-25 10:44:26 -0400

    add cell formatting

--- src/components/ProductListCellView.js
+++ src/components/ProductListCellView.js
@@ -8,17 +8,26 @@ type Props = { product: Product };
 const ProductListCellView = ({ product }: Props) => {
   return (
     <View style={styles.cellContainer}>
-      <Text> {product.name} </Text>
+      <NameText> {product.name} </NameText>
+      <PriceText> ${product.price} </PriceText>
     </View>
   );
 };
 export default ProductListCellView;
+
 const styles = {
   cellContainer: {
     borderBottomWidth: 0.5,
-    width: "100%",
+    borderBottomColor: "grey",
+    width: "95%",
     height: "10%",
-    paddingVertical: 10,
-    paddingHorizontal: 5
+    paddingVertical: 10
   }
 };
+const baseSize = 14;
+const text = {
+  name: { fontSize: baseSize + 4, fontWeight: "bold" },
+  price: { fontSize: baseSize + 2, color: "green" }
+};
+const NameText = props => <Text style={text.name}>{props.children}</Text>;
+const PriceText = props => <Text style={text.price}>{props.children}</Text>;
