493605e73fc6b0fd4351e50cc10faa958cdd7cd1
tuzmusic <tuzmusic@gmail.com>
2019-07-25 11:11:06 -0400

    render images in cells

--- src/components/ProductListCellView.js
+++ src/components/ProductListCellView.js
@@ -1,13 +1,20 @@
 // @flow
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { View, Text, ImageBackground } from "react-native";
+import { Image } from "react-native-elements";
 import Product from "../models/Product";
 
 type Props = { product: Product };
 
 const ProductListCellView = ({ product }: Props) => {
+  console.log(product.images[0].src);
+
   return (
     <View style={styles.cellContainer}>
+      <Image
+        source={{ uri: product.images[0].src }}
+        style={styles.backgroundImage}
+      />
       <NameText> {product.name} </NameText>
       <PriceText> ${product.price} </PriceText>
     </View>
@@ -19,9 +26,16 @@ const styles = {
   cellContainer: {
     borderBottomWidth: 0.5,
     borderBottomColor: "grey",
-    width: "95%",
-    height: "10%",
-    paddingVertical: 10
+    width: "100%",
+    height: "11%"
+    // paddingVertical: 10
+  },
+  backgroundImage: {
+    width: "120%",
+    height: "120%",
+    backgroundColor: "lightblue",
+    justifyContent: "center",
+    alignItems: "center"
   }
 };
 const baseSize = 14;
