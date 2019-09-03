b55f3ad2e35fa8379f50acefb9048a32eb534769
tuzmusic <tuzmusic@gmail.com>
2019-09-03 13:07:50 -0400

    re-design quantity icons

--- src/components/Quantity.js
+++ src/components/Quantity.js
@@ -3,22 +3,27 @@ import React from "react";
 import { Text, View } from "react-native";
 import { Input, Icon } from "react-native-elements";
 
-type Props = { value: string, onChange: number => void };
+type Props = {
+  value: string,
+  onChange: number => void,
+  label?: string,
+  showLabel?: boolean
+};
 
 const Quantity = (props: Props) => {
+  Quantity.defaultProps = { showLabel: true };
   function onChange(n) {
     const newVal = Number(props.value) + n;
     if (newVal >= 0) props.onChange(newVal);
   }
 
   const iconProps = {
-    size: 30,
-    type: "evilicon",
-    color: "grey"
+    size: 15,
+    type: "antdesign"
   };
   return (
     <View style={styles.superContainer}>
-      <Text style={styles.text}>Quantity:</Text>
+      {props.showLabel && <Text style={styles.text}>Quantity:</Text>}
       <View style={styles.row}>
         <Icon name="minus" onPress={() => onChange(-1)} {...iconProps} />
         <Input inputStyle={styles.input} value={props.value} />
