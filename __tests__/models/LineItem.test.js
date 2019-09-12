43bd4911a1204808c8f3c5387b9cc3ea626033ae
tuzmusic <tuzmusic@gmail.com>
2019-09-12 17:02:26 -0400

    write LineItem.toOrderApi method

---
+++ __tests__/models/LineItem.test.js
@@ -0,0 +1,108 @@
+// @flow
+
+import CheckboxesField from "../../src/models/fields/CheckboxesField";
+import SelectboxField from "../../src/models/fields/SelectboxField";
+import LineItem from "../../src/models/LineItem";
+import SubscriptionProduct from "../../src/models/SubscriptionProduct";
+import { subscriptionProducts } from "../../__mocks__/subscription-products-response";
+describe("LineItem.toOrderApi", () => {
+  it("converts a LineItem object for use in POSTing an order", () => {
+    expect(LineItem.toOrderApi(lineItem)).toEqual(
+      subscriptionWithFreeAndExtraObj
+    );
+  });
+});
+
+const product = SubscriptionProduct.fromApi(subscriptionProducts[0]);
+
+const lineItem = Object.assign(new LineItem(), {
+  product,
+  quantity: 1,
+  items: [
+    {
+      selections: [
+        { name: "Twix", quantity: 1, price: 0 },
+        { name: "Snickers", quantity: 1, price: 0 },
+        { name: "Butterfinger", quantity: 2, price: 0 }
+      ],
+      fieldName: "Have some free candies"
+    },
+    {
+      selections: [
+        { name: "Twix", quantity: 1, price: 5 },
+        { name: "M&M", quantity: 2, price: 5 }
+      ],
+      fieldName: ""
+    },
+    {
+      card:
+        "An anniversary is a reminder as to why you love and married this person.  -Zoe Foster Blake",
+      fieldName: "Add a Anniversary Gift Card"
+    }
+  ]
+});
+
+// Base object!!!
+const singleCandyObj = {
+  product_id: product.id,
+  quantity: 2
+};
+
+const subscriptionWithFreeAndExtraObj = {
+  product_id: product.id, // can be whatever for the test. the back-end api will take it from there.
+  quantity: 1, // can be whatever for the test. the api will take it from there.
+  meta_data: [
+    {
+      key: "_tmcartepo_data",
+      value: [
+        /* Free candy 1 */ {
+          name: "Have some free candies",
+          value: "Twix",
+          price: 0,
+          section: "",
+          quantity: 1
+        },
+        /* Free candy 2 */ {
+          name: "Have some free candies",
+          value: "Snickers",
+          price: 0,
+          section: "",
+          quantity: 1
+        },
+        /* Free candy 3&4 */ {
+          name: "Have some free candies",
+          value: "Butterfinger",
+          price: 0,
+          section: "",
+          quantity: 2
+        },
+        /* Extra candy 1 */ {
+          name: "",
+          value: "Twix",
+          price: 5,
+          section: "",
+          quantity: 1
+        },
+        /* Extra candy 2&3 */ {
+          name: "",
+          value: "M&M",
+          price: 10,
+          section: "",
+          quantity: 2
+        },
+        /* Selected card */ {
+          name: "Add a Anniversary Gift Card",
+          value:
+            "An anniversary is a reminder as to why you love and married this person.  -Zoe Foster Blake",
+          price: 0,
+          section: "",
+          quantity: 1
+        }
+      ]
+    },
+    /* not necessarily needed; but, for the test we'll say this subscription is $10 each */ {
+      key: "_tm_epo_product_original_price",
+      value: ["20"]
+    }
+  ]
+};
---
+++ src/models/LineItem.js
@@ -0,0 +1,116 @@
+// @flow
+import * as Types from "../redux/FormTypes";
+import ShopWorker from "./ShopWorker";
+import Product from "./Product";
+
+type CardSelection = {
+  card: string,
+  fieldName: string
+};
+
+type CheckboxesSelection = {
+  selections: Types.QuantifiedOrderItem[],
+  fieldName: string
+};
+
+type OrderSelection = CardSelection | CheckboxesSelection;
+
+class LineItem {
+  product: Product;
+  quantity: number;
+  items: OrderSelection[];
+
+  static toOrderApi(lineItem: LineItem): Object {
+    const { product, quantity } = lineItem;
+    const obj: ToOrderApiObject = {
+      product_id: product.id,
+      quantity
+    };
+
+    if (lineItem.items) {
+      const values: MetaDataValueObject[] = [];
+
+      for (let item of lineItem.items) {
+        /* 
+          const value = {};
+          value.name = null;
+          value.value = null;
+          value.price = null;
+          value.section = "";
+          value.quantity = null;
+         */
+
+        if (item.card) {
+          /* 
+            value.quantity = 1;
+            value.price = 0;
+            value.name = item.fieldName;
+            value.value = item.card;
+            value.section = "";
+
+            values.push(value) 
+            */
+          values.push({
+            quantity: 1,
+            price: 0,
+            name: item.fieldName,
+            value: item.card,
+            section: ""
+          }); // hold off on this flow error until the test passes
+        } else if (item.selections) {
+          const selections: Types.QuantifiedOrderItem[] = item.selections; // hold off on this flow error until the test passes
+
+          for (let selection of selections) {
+            const { quantity, price, name } = selection;
+            values.push({
+              quantity,
+              price: (price || 0) * quantity,
+              name: item.fieldName,
+              value: name,
+              section: ""
+            });
+          }
+        }
+      }
+
+      const formInfo: FormInfoObject = {
+        key: "_tmcartepo_data",
+        value: values
+      };
+
+      const origPriceInfo: OriginalPriceObject = {
+        key: "_tm_epo_product_original_price",
+        value: [product.price.toString()]
+      };
+
+      obj.meta_data = [formInfo, origPriceInfo];
+    }
+    return obj;
+  }
+}
+
+export default LineItem;
+
+type MetaDataValueObject = {
+  name: string,
+  value: string,
+  price: number,
+  section: "",
+  quantity: number
+};
+
+type ToOrderApiObject = {|
+  product_id: number,
+  quantity: number,
+  meta_data?: [FormInfoObject, ?OriginalPriceObject]
+|};
+
+type FormInfoObject = {
+  key: "_tmcartepo_data",
+  value: MetaDataValueObject[]
+};
+
+type OriginalPriceObject = {
+  key: "_tm_epo_product_original_price",
+  value: [string]
+};
